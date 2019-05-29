7f906580ee287095efcd0f2489f0c7c4cf728653
xNoll <36823049+xNoll@users.noreply.github.com>
2019-05-29 19:32:02 +0200

    Update ControlDonacion.java

--- NetBeansProjects/AcoesEE/AcoesEE-war/src/java/control/ControlDonacion.java
+++ NetBeansProjects/AcoesEE/AcoesEE-war/src/java/control/ControlDonacion.java
@@ -1,20 +1,32 @@
 package control;
 
+//import java.io.Serializable;
+//import javax.enterprise.context.SessionScoped;
+//import javax.inject.Named;
+
 import entidades.Donacion;
+import java.util.List;
 import entidades.Nino;
 import entidades.Socio;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
+//import java.util.Date;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import negocio.NegocioGenerico;
 
 @Named(value = "ControlDonacion")
 @SessionScoped
 public class ControlDonacion implements Serializable {
     private Donacion donacion;
-    private ArrayList<Donacion> donaciones;
-
+    //private ArrayList<Donacion> donaciones;
+    
+    @EJB
+    private NegocioGenerico neg;
     private Socio socio;
     private Nino nino;
     
@@ -22,71 +34,49 @@ public class ControlDonacion implements Serializable {
     
     public ControlDonacion() {
         donacion = new Donacion();
-        
-        this.year = 1;
-        this.month = 1;
-        this.day = 1;
-        
-        this.socio = new Socio();
-        this.nino = new Nino();
-        
+        /*  
         donaciones = new ArrayList<>();
         donaciones.add(new Donacion(new Socio("898928W", "Álvaro", "Ramírez"), new Nino("Marina", "Balmén", 'M', new Date(2003 - 1900, 1 - 1, 20)), new Date(2018-1900, 10, 3), 120.0, "Todo bien"));
+        */
     }
     
     public String addDonacion() {
-        this.donacion.setSocio(this.socio);
-        this.donacion.setNino(this.nino);
-        this.donacion.setFecha(new Date(this.year - 1900, this.month - 1, this.day));
-        
-        donaciones.add(this.donacion);
+        try{
+            neg.add(this.donacion);
+        } catch(EJBException e){
+            FacesContext ctx = FacesContext.getCurrentInstance();
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,e.getMessage(),e.getMessage()));
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Probablemente hay algún campo de tipo numérico incorrecto o algún campo está incompleto", null));
+        }
         
         this.donacion = new Donacion();
-        this.socio = new Socio();
-        this.nino = new Nino();
-        
-        this.year = 1;
-        this.month = 1;
-        this.day = 1;
         
         return null;
     }
     
     public String removeDonacion(Donacion don) {
-        donaciones.remove(don);
+        neg.remove(don);
         
         return null;
     }
     
     public String goModifyDonacion(Donacion don) {
-        this.nino = don.getNino();
-        this.socio = don.getSocio();
-        
-        this.year = don.getFecha().getYear();
-        this.month = don.getFecha().getMonth();
-        this.day = don.getFecha().getDay();
-        
         this.donacion = don;
         
         return "donacionesModificar.xhtml";
     }
     
     public String modifyDonacion() {
-        for(Donacion don: donaciones) {
-            if(don.equals(this.donacion)) {
-                don = this.donacion;
-                don.setFecha(new Date(this.year - 1900, this.month - 1, this.day));
-            }
-        }
+        try{
+            for(Object don: neg.getRows("getDonaciones"))
+                if(don.equals(this.donacion)) neg.modify(this.donacion);
+        }catch(EJBException e){
+            FacesContext ctx = FacesContext.getCurrentInstance();
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Probablemente hay algún campo de tipo numérico incorrecto o algún campo está incompleto", null));
+        } 
         
-        this.donacion = new Donacion();
-        this.socio = new Socio();
-        this.nino = new Nino();
-        this.year = 1;
-        this.month = 1;
-        this.day = 1;
-        
-        
+        this.donacion=new Donacion();
         return "donaciones.xhtml";
     }
     
@@ -103,14 +93,14 @@ public class ControlDonacion implements Serializable {
         this.donacion = donacion;
     }
 
-    public ArrayList<Donacion> getDonaciones() {
-        return donaciones;
+    public List<Donacion> getDonaciones() {
+        return neg.getRows("getDonaciones");
     }
-
+    /*
     public void setDonaciones(ArrayList<Donacion> donaciones) {
         this.donaciones = donaciones;
     }
-
+    */
     public int getYear() {
         return year;
     }
@@ -152,3 +142,4 @@ public class ControlDonacion implements Serializable {
     }
     
 }
+
