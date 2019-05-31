dc9f1608dafc2ff34dcfee4ef05b9c4c57b4c7dd
iTiago98 <itiago98@gmail.com>
2019-05-31 12:51:46 +0200

    Beneficiario funciona (falta perfilar una cosilla)

--- NetBeansProjects/AcoesEE/AcoesEE-war/src/java/control/ControlBeneficiario.java
+++ NetBeansProjects/AcoesEE/AcoesEE-war/src/java/control/ControlBeneficiario.java
@@ -7,7 +7,11 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import entidades.Beneficiario;
 import entidades.Nino;
+import java.util.ArrayList;
 import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import negocio.NegocioGenerico;
 
 @Named(value = "ControlBeneficiario")
@@ -27,14 +31,36 @@ public class ControlBeneficiario implements Serializable {
     }
     
     public String addBeneficiario() {
-        this.beneficiario.setNino(this.nino);
-        this.beneficiario.setBeca(this.beca);
-        neg.add(this.beneficiario);
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        
+        List<Nino> ln = new ArrayList<>();
+        List<Beca> lb = new ArrayList<>();
+        
+        String ninoQuery = "SELECT n FROM Nino n WHERE upper(n.nombre) = upper(\'" + this.nino.getNombre() + "\') and upper(n.apellidos) = upper(\'" + this.nino.getApellidos() + "\')";
+        String becaQuery = "SELECT b FROM Beca b WHERE upper(b.nombre) = upper(\'" + this.beca.getNombre() + "\')";
+        
+        if(this.nino.getId() != null) ln = neg.getRowById("Nino", this.nino.getId());
+        if(this.beca.getId() != null) lb = neg.getRowById("Beca", this.beca.getId());
+        
+        try {
+            // 10x (bis)
+            if(ln.size() == 1 || (ln = neg.getRowsCustomQuery(ninoQuery)).size() == 1) this.beneficiario.setNino(ln.get(0));
+            else throw new EJBException("La búsqueda del niño en la base de datos ha devuelto un número de resultados distinto del que se esperaba (!= 1)");
+        
+            if(lb.size() == 1 || (lb = neg.getRowsCustomQuery(becaQuery)).size() == 1) this.beneficiario.setBeca(lb.get(0));
+            else throw new EJBException("La búsqueda de la beca en la base de datos ha devuelto un número de resultados distinto del que se esperaba (!= 1)");
+        
+            neg.add(this.beneficiario);
+            
+        } catch (EJBException e){
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Pruebe a ser más preciso con la búsqueda, rellenando el campo id", null));
+        }
         
         this.beneficiario = new Beneficiario();
         this.nino = new Nino();
         this.beca = new Beca();
-
+        
         return null;
     }
     
@@ -53,9 +79,10 @@ public class ControlBeneficiario implements Serializable {
     }
     
     public String modifyBeneficiario() {
-        for(Object b: neg.getRows("getBeneficiarios")) {
+        /*for(Object b: neg.getRows("getBeneficiarios")) {
             if(b.equals(this.beneficiario)) neg.modify(this.beneficiario);
-        }
+        }*/
+        neg.modify(this.beneficiario);
         
         this.beneficiario = new Beneficiario();
         this.nino = new Nino();
